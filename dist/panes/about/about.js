96878887009af85005c822630dac13153651d3d4
Veronique Pelletier <vpelletier@lvlstudio.com>
2013-11-24 17:46:58 -0500

    deleting unsed files

--- dist/panes/about/about.js
+++
@@ -1,6 +0,0 @@
-angular.module('app.panes.about', []).config(['$routeProvider', function ($routeProvider) {
-    $routeProvider.when('/about', {
-        templateUrl: '/panes/about/about.html',
-        scope: true
-    });
-}]);
--- dist/panes/home/home.js
+++
@@ -1,6 +0,0 @@
-angular.module('app.panes.home', []).config(['$routeProvider', function ($routeProvider) {
-    $routeProvider.when('/home', {
-        templateUrl: '/panes/home/home.html',
-        scope: true
-    });
-}]);
